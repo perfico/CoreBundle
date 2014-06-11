26ea2f962111f26d1899d697783433c5d34cfd78
Alex Sidorov <asidorov01@gmail.com>
2014-06-11 20:38:20 +0400

    reflection helper

---
+++ Util/ReflectionHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Perfico\CoreBundle\Util;
+
+/**
+ * Class ReflectionHelper
+ * @package Perfico\CoreBundle\Util
+ */
+class ReflectionHelper
+{
+
+    /**
+     * Return filtered constants of class
+     *
+     * @param $class
+     * @param $regex
+     * @return array
+     */
+    static public function getConstants($class, $regex)
+    {
+        $r = new \ReflectionClass($class);
+
+        $filteredKeys = array_filter(array_keys($r->getConstants()), function($const) use ($regex) {
+            return preg_match($regex, $const);
+        });
+
+        $result = array();
+        foreach ($filteredKeys as $filteredKey) {
+            $result[$r->getConstant($filteredKey)] = $r->getConstant($filteredKey);
+        }
+
+        return $result;
+    }
+}
